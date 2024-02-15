38a86945691386a73a00d5fc26b17005c60d75d5
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2024-02-15 17:33:41 -0500

    fixed landing gallery

--- client/src/components/FeaturedGallery/index.jsx
+++ client/src/components/FeaturedGallery/index.jsx
@@ -6,15 +6,15 @@ const FeaturedGallery = () => {
   const images = [
     {
       url: "https://firebasestorage.googleapis.com/v0/b/artsquare-bb6b4.appspot.com/o/images%2F657167fc01d8362bba39738c%2F2023-12-07T06%3A37%3A06.205Z_5230c665-56f4-412d-984c-6f8d0691b8c1?alt=media&token=ce23afb4-f03c-406a-b803-e969c7079404",
-      title: "Image Title 1",
+      title: "Band Practice",
     },
     {
       url: "https://firebasestorage.googleapis.com/v0/b/artsquare-bb6b4.appspot.com/o/images%2F65712ea0bf130e68dc460d2a%2F2023-12-07T06%3A35%3A09.720Z_3c8e74a4-2685-427f-8d42-b756c8a1f414?alt=media&token=dcbf8bf1-4599-40dd-ba20-2f8a70babde1",
-      title: "Image Title 1",
+      title: "We The Wild",
     },
     {
       url: "https://firebasestorage.googleapis.com/v0/b/artsquare-bb6b4.appspot.com/o/images%2F65712ea0bf130e68dc460d2a%2F2023-12-07T06%3A34%3A43.125Z_7772715a-4bbf-4694-93d3-644a79b3e2fc?alt=media&token=cedacfcb-374f-4145-b9fe-ab5434509062",
-      title: "Image Title 1",
+      title: "Random Number Gods",
     },
   ];
   useEffect(() => {
